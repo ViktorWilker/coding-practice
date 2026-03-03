cf8e8bef9f06726c74e4eafbf23bcb9cc62566da
ViktorWilker <143961500+ViktorWilker@users.noreply.github.com>
2026-03-02 21:51:48 -0300

    Add Kotlin Exercices

---
+++ Kotlin/Lista_01/HoraDeCodar_02.kt
@@ -0,0 +1,5 @@
+fun main(){
+    //Escreva um programa em que o usuário informe o seu nome e exiba a mensagem "Olá, [NomeDoUsuario]".
+    var nomeDoUsuario = readln()
+    print("Olá $nomeDoUsuario")
+}
