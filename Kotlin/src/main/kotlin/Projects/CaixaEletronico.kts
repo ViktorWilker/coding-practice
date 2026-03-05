e8e3bf694848536590066a2aa849423e6b2fd227
ViktorWilker <143961500+ViktorWilker@users.noreply.github.com>
2026-03-05 12:52:37 -0300

    w.i.p

--- Kotlin/src/main/kotlin/Projects/CaixaEletronico.kts
+++ Kotlin/src/main/kotlin/Projects/CaixaEletronico.kts
@@ -1,41 +1,44 @@
-
-
 class User(val userName: String, private val password: Int = 3589) {
-    fun checkPassword(typed: Int): Boolean = typed == password
+    fun checkPassword(): Boolean {
+        var tryied = 3
+        while (tryied > 0) {
+            print("Digite a senha: ")
+            val typed = readln().toInt()
+
+            if (typed == password) {
+                return true
+            }
+
+            println("!!Senha incorreta!!")
+            tryied--
+            println("Tentativas restantes: $tryied")
+        }
+
+        println("Acesso bloqueado...")
+        return false
+    }
 }
 
-    var
-    var bLogged = false
+main()
+fun main(){
 
-    if(!bLogged){
-        if(!Login()) return
-        bLogged = true
+    var bLogged = false
+    lateinit var currentUser: User
+
+    if (!bLogged){
+        currentUser = Login()
     }
 
-
-println("Bem-vindo ao banco digital")
-
-
-    println("Olá usuário, é um prazer ter você aqui")
+    println("Bem-vindo ao banco digital")
+    println("Olá ${currentUser.userName}, é um prazer ter você aqui")
     println("[1]Saldo - [2]Extrato - [3]Saque - [4]Depósito - [5]Transferência - [6]Sair")
+}
 
-
-
-
-
-
-
-
-
-
-
-
-fun Login(): Boolean {
+fun Login(): User {
     print("Digite seu nome: ")
     val name = readln()
-    val current_user = User(name)
-    print("Digite a senha: ")
-    val password = readln().toInt()
 
-    return current_user.checkPassword(password)
+    val currentUser = User(name)
+
+    return currentUser
 }
