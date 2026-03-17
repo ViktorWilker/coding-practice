06aa86c48498e4c178212a6f7afdc7d6c1555e0c
ViktorWilker <143961500+ViktorWilker@users.noreply.github.com>
2026-03-16 23:04:06 -0300

    caixa eletronico finished

--- Kotlin/src/main/kotlin/Projects/CaixaEletronico.kts
+++ Kotlin/src/main/kotlin/Projects/CaixaEletronico.kts
@@ -1,4 +1,4 @@
-class User(val userName: String, private val password: Int = 3589) {
+class User(val userName: String, val password: Int = 3589) {
     fun checkPassword(): Boolean {
         var tryied = 3
         while (tryied > 0) {
@@ -17,28 +17,84 @@ class User(val userName: String, private val password: Int = 3589) {
         println("Acesso bloqueado...")
         return false
     }
-}
 
-main()
-fun main(){
+    private var saldo = 0f
+    fun showSaldo(){if (checkPassword()){println(saldo)
+    backToHome()}else{return}}
+    fun addSaldo(value: Float){if (checkPassword()) {saldo += value
+    backToHome()}else{return} }
+    fun saque(value: Float){if (checkPassword()) {saldo -= value
+    backToHome()}else{return}}
 
-    var bLogged = false
-    lateinit var currentUser: User
-
-    if (!bLogged){
-        currentUser = Login()
     }
 
-    println("Bem-vindo ao banco digital")
-    println("Olá ${currentUser.userName}, é um prazer ter você aqui")
-    println("[1]Saldo - [2]Extrato - [3]Saque - [4]Depósito - [5]Transferência - [6]Sair")
+main()
+var bLogged = false
+lateinit var currentUser: User
+var run = true
+fun main() {
+
+    println("!!Bem vindo ao seu banco digital!!")
+    currentUser = Login()!!
+    home()
 }
 
-fun Login(): User {
+fun home(){
+
+    while (run) {
+        var opc = 0
+        println("Bem-vindo ao banco digital")
+        println("Olá ${currentUser.userName}, é um prazer ter você aqui")
+        println("[1]Saldo - [2]Extrato - [3]Saque - [4]Depósito - [5]Transferência - [6]Sair")
+
+        opc = readln().toInt()
+        when (opc) {
+            1 -> currentUser.showSaldo()
+            2 -> currentUser.extrato()
+            3 -> currentUser.saque(readln().toFloat())
+            4 -> currentUser.addSaldo(readln().toFloat())
+            5 -> currentUser.transf()
+            else -> ""
+        }
+    }
+}
+
+fun Login(): User? {
     print("Digite seu nome: ")
     val name = readln()
-
     val currentUser = User(name)
+    if(!currentUser.checkPassword()){return null}
 
     return currentUser
+}
+fun User.extrato(){
+    if(!checkPassword()){return}
+    backToHome()
+}
+
+fun User.transf(){
+    if(!currentUser.checkPassword()){return}
+    print("Informe o número da conta para transferir(4 dígitos):")
+    val x = readln().toInt()
+    if(x > 9999 || x < 1000){erro("conta inválida!Tente novamente mais tarde")}
+    print(" Digite o valor a ser transferido: ")
+    val value = readln().toFloat()
+    currentUser.saque(value)
+    backToHome()
+}
+
+fun erro(msg: String){
+    println(msg)
+    main()
+}
+
+fun backToHome(){
+    var opc = 0
+    println("Voltar para o menu?(menu[1] - sair[2]")
+    opc = readln().toInt()
+    if(opc == 2){
+        run = false
+    }
+
+    home()
 }
